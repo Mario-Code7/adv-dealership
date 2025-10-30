8debf2008cc6ef66f0101b722ae10b7c4a6759a1
Mario-Code7 <marioroa71@yahoo.com>
2025-10-30 15:59:06 -0500

    added printwriter in ContractDataManager class

--- src/main/java/com/yearup/dealership/ContractDataManager.java
+++ src/main/java/com/yearup/dealership/ContractDataManager.java
@@ -7,14 +7,44 @@ import java.io.PrintWriter;
 public class ContractDataManager {
 
     public void saveContract(Contract contract) {
-//            try(PrintWriter printWriter = new PrintWriter(new FileWriter("contracts.csv")) {
-//                printWriter.println(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhone());
-//                for (Vehicle vehicle: dealership.getAllVehicles()) {
-//                    printWriter.println(vehicle.getVin() + "|" + vehicle.getYear() + "|" + vehicle.getMake() + "|" + vehicle.getModel() + "|" + vehicle.getVehicleType() + "|" + vehicle.getColor() + "|" + vehicle.getOdometer() + "|" + vehicle.getPrice());
-//                }
-//            } catch (IOException e) {
-//                System.out.println("Error saving your dealership file details: " + e.getMessage());
-//            }
-//        }
+        try(PrintWriter printWriter = new PrintWriter(new FileWriter("contracts.csv", true))) {
+            Vehicle vehicle = contract.getVehicleSold();
+
+            if (contract instanceof SalesContract sale) {
+                printWriter.printf("SALE | %s | %s | %s | %d | %d | %s | %s | %s | %s | %d | %.2f | %.2f | %.2f | %.2f | %.2f | %s | %.2f",
+                        sale.getDate(),
+                        sale.getCustomerName(),
+                        sale.getCustomerEmail(),
+                        vehicle.getVin(),
+                        vehicle.getYear(),
+                        vehicle.getMake(),
+                        vehicle.getModel(),
+                        vehicle.getVehicleType(),
+                        vehicle.getColor(),
+                        vehicle.getOdometer(),
+                        vehicle.getPrice(),
+                        vehicle.getPrice() * 0.05, 100.0,
+                        sale.getProcessingFee(), sale.getTotalPrice(), sale.isFinanceOption(), sale.getMonthlyPayment());
+            } else if (contract instanceof LeaseContract lease) {
+                printWriter.printf("LEASE | %s | %s | %s | %d | %d | %s | %s | %s | %s | %d | %.2f | %.2f | %.2f | %.2f | %.2f%n",
+                        lease.getDate(),
+                        lease.getCustomerName(),
+                        lease.getCustomerEmail(),
+                        vehicle.getVin(),
+                        vehicle.getYear(),
+                        vehicle.getMake(),
+                        vehicle.getModel(),
+                        vehicle.getVehicleType(),
+                        vehicle.getColor(),
+                        vehicle.getOdometer(),
+                        vehicle.getPrice(),
+                        lease.getExpectedEndingValue(),
+                        lease.getLeaseFee(),
+                        lease.getTotalPrice(),
+                        lease.getMonthlyPayment()
+                        );
+
+            }
+        }
     }
 }
